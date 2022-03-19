52ac3ca2238c1ea886df3da9cc547011e36482f0
Lohane Dafne Borges Vieira <lohanedafnevieira@gmail.com>
2022-03-18 23:54:18 -0300

    Add files via upload

---
+++ src/components/Carrosel/carrossel.jsx
@@ -0,0 +1,75 @@
+import "../Carrosel/carrossel.css";
+
+export default () => {
+    return (
+
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col h">
+        <div class="card M">
+          <img class="img-top"/>
+          <div class="card-body">
+            <br></br>
+            <h5 class="m card container">KIT ACAMPAMENTO</h5>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <p class="card text">Temos diversos kit para acampamento completos tanto para locação como para venda.
+            <br></br>
+            <a href="http://localhost:3000/Contato" > SAIBA MAIS</a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col h">
+        <div class="card B">
+          <img class="img-top"/>
+          <div class="card-body">
+            <br></br>
+            <h5 class="m card container">BARRACAS</h5>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <p class="card text">Se você vai viajar sozinho ou em grupo, temos barracas de todos os tamanhos.
+            <br></br>
+            <a href="http://localhost:3000/Contato" > SAIBA MAIS</a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col h">
+        <div class="card P">
+          <img class="img-top"/>
+          <div class="card-body">
+            <br></br>
+            <h5 class="m card container">PASSEIOS</h5>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <p class="card text">Passeios exclusivos para visitar Grutas, Cachoeiras, Montanhas, Pôr do Sol e muito mais
+            <br></br>
+            <a href="http://localhost:3000/Contato" > SAIBA MAIS</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    );
+}
